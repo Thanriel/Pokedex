579e5ec2b9b8e50d7d2f12f09efd0da84a392811
Thales <59477603+Thanriel@users.noreply.github.com>
2022-10-25 21:40:31 -0300

    Commit

--- js/app.js
+++ js/app.js
@@ -1,45 +1,57 @@
-const url = `https://pokeapi.co/api/v2/pokemon/`
+//Constante que armazena a ur
+const getPokemon = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
+const generatePokemonPromisses = () => Array(151).fill().map((_, index) =>
+    fetch(getPokemon(index + 1)).then(response => response.json()))
 
-const fetchPokemon = () => {
-    const getPokemon = id => `${url}${id}`
+const generateHTML = pokemons => pokemons.reduce((accumullator, { id, name, types }) => {
 
-    const pokemonPromises = [] 
+    const elementTypes = types.map(typeInfo => typeInfo.type.name)
 
-    for (let i = 1; i <= 150; i++) {
-        pokemonPromises.push(fetch(getPokemon(i)).then(response => response.json()))
+    if (elementTypes[1] === undefined) {
+        accumullator +=
+            `
+            <li class="card-primario ${elementTypes[0]}-back">
+                <img class="card-image" alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" onclick="getUniquePokemon(${id})"}>
+                <h2 clas="name">#${id}  ${name}</h2>
+                <span class="card-subtitle type ${elementTypes[0]}">${elementTypes[0]}
+            </li>
+            `
+        return accumullator
+    }
+    else {
+        accumullator +=
+            `
+            <li class="card-primario ${elementTypes[0]}-back">
+                <img class="card-image" alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" onclick="getUniquePokemon(${id})"}>
+                <h2 clas="name">#${id}  ${name}</h2>
+                <span class="card-subtitle type ${elementTypes[0]}">${elementTypes[0]}</span><span card-subtitle class="${elementTypes[1]} type">${elementTypes[1]}</span>
+            </li>
+            `
+        return accumullator
     }
 
-    Promise.all(pokemonPromises)
-        .then(pokemons => {
-            const lisPokemons = pokemons.reduce((accumullator, pokemon) => {
-                
-                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+}, '')
 
-                accumullator += 
-                    `
-                    <li class="card-primario" onclick="getUniquePokemon(${pokemon.id})">
-                        <img class="card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" ${onclick="console.log('teste')"}>
-                        <h2 clas="name">#${pokemon.id}  ${pokemon.name}</h2>
-                        <span class="card-subtitle type ${types[0]}">${types[0]}</span><span card-subtitle class="${types[1]} type">${types[1]}</span>
-                    </li>
-                    `
-                return accumullator
-            }, '')
-
-            const ul = document.querySelector('[data-js="pokedex"]')
-
-            ul.innerHTML = lisPokemons
-        })
+const insertPokemonsIntoPage = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]')
+    ul.innerHTML = pokemons
 }
 
 
-fetchPokemon()
+const pokemonPromises = generatePokemonPromisses()
+
+Promise.all(pokemonPromises)
+    .then(generateHTML)
+    .then(insertPokemonsIntoPage)
 
 
-
-function getUniquePokemon (id) {
+function getUniquePokemon(id) {
     location.href = "poke-info.html"
     const getPokemon = `${url}${id}`
+
+    const pokemon = fetch(getPokemon).then(response => response.json())
+
+    console.log(pokemon.name)
 
 }
